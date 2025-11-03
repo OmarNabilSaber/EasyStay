5c638ab7a0fc64fe653d726c8d5838c74cf65359
omar.nabil <omar.nabil@DESKTOP-1H3DOLI>
2025-11-03 09:53:47 +0200

    Add project files.

---
+++ EasyStay.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyStay.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
