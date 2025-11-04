7c63c357293d3755e7282698756510b67c0801c6
OmarNabil <omarnabilsaber58@gmail.com>
2025-11-04 17:39:43 +0200

    Adding villa model, and index view

---
+++ EasyStay.Web/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using EasyStay.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyStay.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
