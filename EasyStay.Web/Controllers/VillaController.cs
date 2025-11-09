c0fa2d28766b28a7c5298181388038b5e28de3d1
OmarNabil <omarnabilsaber58@gmail.com>
2025-11-09 17:07:50 +0200

    add update, delete end points and views

--- EasyStay.Web/Controllers/VillaController.cs
+++ EasyStay.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using EasyStay.Infrastructure.Data;
+﻿using EasyStay.Domain.Entities;
+using EasyStay.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyStay.Web.Controllers
@@ -10,10 +11,78 @@ namespace EasyStay.Web.Controllers
         {
             _db = db;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             var villas = _db.Villas.ToList();
             return View(villas);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(villa);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully";
+                return RedirectToAction("Index","Villa");
+            }
+            TempData["error"] = "The villa can't be created";
+            return View(villa);
+        }
+        [HttpGet]
+        public IActionResult Update(int villaId)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (villa is null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(villa);
+        }
+        [HttpPost]
+        public IActionResult Update(Villa villa)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(villa);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "The villa can't be updated";
+            return View(villa);
+        }
+        [HttpGet]
+        public IActionResult Delete(int villaId)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (villa is null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(villa);
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa? villaFromDb = _db.Villas.FirstOrDefault(v => v.Id == villa.Id);
+
+            if (villaFromDb is not null)
+            {
+                _db.Villas.Remove(villaFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "The villa can't be deleted";
+            return View(villa);
         }
     }
 }
